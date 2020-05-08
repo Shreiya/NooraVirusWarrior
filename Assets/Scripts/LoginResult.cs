1321505f5b58cb3620cf6adc3264421b05e39981
Mayank Raj <AgrMayank@outlook.com>
2020-05-08 20:02:58 +0530

    Add Name on Login

--- Assets/Scripts/LoginResult.cs
+++ Assets/Scripts/LoginResult.cs
@@ -9,8 +9,8 @@ public class LoginResult : MonoBehaviour
     public Text LoginResultText;
     void Start()
     {
-        var userName = PlayerPrefs.GetString("U_NAME", "Unknown");
-        LoginResultText.text = "Welcome " + userName + " to GoCorona Game!!";
+        var userName = PlayerPrefs.GetString("U_NAME");
+        LoginResultText.text = "Hi! " + userName;
         Debug.LogFormat("Successfully signed in as {0}", userName);
     }
 }
