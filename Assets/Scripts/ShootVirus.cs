05f9691c1208cd3b511181141ea0049581f72afd
Mayank Raj <AgrMayank@outlook.com>
2020-05-08 16:16:02 +0530

    Add Gamesound, Slash sound (3D)

--- Assets/Scripts/ShootVirus.cs
+++ Assets/Scripts/ShootVirus.cs
@@ -7,43 +7,10 @@ public class ShootVirus : MonoBehaviour
 
     public GameObject SlashAnim;
     public GameObject MainCamera;
-    //public GameObject smoke;
+    // public AudioSource OnCoronaDestroy;
 
-    // public void Shoot()
-    // {
-    //     RaycastHit hit;
-    //     if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out hit))
-    //     {
-    //         if (hit.transform.name == "CoronaVirus(Clone)" || hit.transform.name == "1CoronaVirus(Clone)" || hit.transform.name == "2CoronaVirus(Clone)")
-    //         {
-    //             ScoreScript.scoreValue += 1;
-    //             Destroy(hit.transform.gameObject);
-    //             //Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
-    //         }
-    //     }
-    // }
-    
     void Update()
     {
-        // RaycastHit hit;
-
-        // for (var i = 0; i < Input.touchCount; ++i)
-        // {
-        //     if (Input.GetTouch(i).phase == TouchPhase.Began)
-        //     {
-        //         Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-        //         if (Physics.Raycast(ray, out hit))
-        //         {
-        //             if (tag == "Enemy")
-        //             {
-        //                 ScoreScript.scoreValue += 1;
-        //                 Destroy(hit.collider.gameObject);
-        //             }
-        //         }
-        //     }
-        // }
-
-
         if (Input.GetMouseButtonDown(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -54,35 +21,18 @@ public class ShootVirus : MonoBehaviour
                 var rig = hitInfo.collider.GetComponent<Rigidbody>();
                 if (rig != null)
                 {
+                    // Play slice Animation
                     var ani = Instantiate(SlashAnim, rig.position, Quaternion.identity) as GameObject;
+
+                    // Destroy Gameobjects
                     Destroy(rig.gameObject, .5F);
                     Destroy(ani.gameObject, .5F);
+                    rig.GetComponent<AudioSource>().Play();
+
+                    // Update Score
                     ScoreScript.scoreValue += 1;
                 }
             }
         }
-        // Slice Animation
-        // Vector3 fingerPos = Input.GetTouch(0).position;
-        // fingerPos.z = 0;
-        // // Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
-        // var ani = Instantiate(SlashAnim, fingerPos, Quaternion.identity) as GameObject;
-        // Destroy(ani, 1);
-        // var cam = MainCamera.GetComponent<Camera>();
-
-        // Vector3 point = new Vector3();
-        // Event currentEvent = Event.current;
-        // Vector2 mousePos = new Vector2();
-
-        // // Get the mouse position from Event.
-        // // Note that the y position from Event is inverted.
-        // mousePos.x = currentEvent.mousePosition.x;
-        // mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-
-        // point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-
-        // Instantiate(SlashAnim, point, Quaternion.identity);
-
-
-        // End of Slice Animation
     }
 }
