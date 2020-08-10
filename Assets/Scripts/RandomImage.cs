f650fb1224109f1dfa632ff9ba12e84cbae8bfea
Mayank Raj <AgrMayank@outlook.com>
2020-08-10 19:33:10 +0530

    Update Scoreboard; Add Randomize image script.

---
+++ Assets/Scripts/RandomImage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomImage : MonoBehaviour
+{
+    public Sprite[] images;
+    public Image imageContainer;
+
+    void Start()
+    {
+        int num = Random.Range(0, images.Length);
+
+        imageContainer.sprite = images[num];
+    }
+}
